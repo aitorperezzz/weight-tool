62ef261584b9c69658062f4cd773c4465b429172
Aitor Perez <aitorpegea@gmail.com>
2025-10-30 20:36:10 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () =>
+  console.log("✅ Server running at http://localhost:3000")
+);
+
